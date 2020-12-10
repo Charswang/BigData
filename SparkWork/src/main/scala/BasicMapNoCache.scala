a49f7c4c933495365812daf88e792a63061be927
charswang <1043594828@qq.com>
2020-12-10 10:13:26 +0300

    为啥提交一次，github上有文件了，但是没内容？

--- SparkWork/src/main/scala/BasicMapNoCache.scala
+++ SparkWork/src/main/scala/BasicMapNoCache.scala
@@ -1,3 +1,25 @@
-object BasicMapNoCache {
+import org.apache.spark.SparkContext
+import org.apache.spark.storage.StorageLevel
 
+/**
+ * 验证有缓存RDD和没有缓存RDD的情况下，执行两个action操作所运行的时间对比
+ * 运行时间几乎相差一倍
+ */
+object BasicMapNoCache {
+  def main(args: Array[String]): Unit = {
+    val master = args.length match{
+      case x:Int if x>0 => args(0)
+      case _ => "local"
+    }
+    val sc = new SparkContext(master,"BasicMapNoCache",System.getenv("SPARK_HOME"))
+    val inputdata = sc.parallelize(List(1,2,3,4,5))
+    val outputdata = inputdata.map(x=>x*x)
+    outputdata.persist(StorageLevel.DISK_ONLY) // 持久化RDD
+    //将执行两次上面计算出来outputdata的过程，因为没有对RDD进行持久化，没有放到内存中
+    val t1 = System.currentTimeMillis()
+    println(outputdata.count())
+    println(outputdata.collect().mkString(","))
+    val t2 = System.currentTimeMillis()
+    println(t2-t1) // 没有持久化RDD的运行时间：1360； 持久化RDD之后的运行时间：677
+  }
 }
--- SparkWork/src/main/scala/PerKeyAvg.scala
+++ SparkWork/src/main/scala/PerKeyAvg.scala
@@ -13,8 +13,13 @@ object PerKeyAvg {
     val sc = new SparkContext(master,"PerKeyAvg",System.getenv("SPARK_HOME"))
     val inputdata = sc.parallelize(List(("coffee",1),("coffee",2),("panda",4)))
     val result = inputdata.combineByKey(v=>(v,1),(x:(Int,Int),y)=>(x._1+y,x._2+1),(x:(Int,Int),y:(Int,Int))=>(x._1+y._1,x._2+y._2))
+    //result是个键值对形式的RDD，所以直接使用result.map(key,xy=>(key,xy._1/xy._2))会出错,
+    //所以在使用map操作的话，要使用case指定元素形式[指明是使用键值对转键值对形式]result.map{case (key,xy)=>(key,xy._1/xy._2)}
     result.map{case (key,xy)=>(key,xy._1/xy._2)}.collect().foreach(println(_))
 //    result.map{case (key,xy)=>(key,xy._1/xy._2)}.collectAsMap().foreach(println(_))
 //    result.map{case (key,xy)=>(key,xy._1/xy._2)}.collectAsMap().map(println(_))
+
+    // 另外不使用map方法，而是直接使用针对键值对中的value进行转换操作的mapValue方法，也可以完成操作。
+    result.mapValues(xy=>xy._1/xy._2.toFloat).collect().foreach(println(_))
   }
 }
